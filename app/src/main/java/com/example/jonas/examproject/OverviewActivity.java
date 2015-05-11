4debf2e3fafe75c1ff2e4594078ae5b0748189ed
Jonas <jons_91@hotmail.com>
2015-05-11 14:17:48 +0200

    Test data now moved to DB and not only to local ArrayList

--- app/src/main/java/com/example/jonas/examproject/OverviewActivity.java
+++ app/src/main/java/com/example/jonas/examproject/OverviewActivity.java
@@ -4,6 +4,7 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Message;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,6 +17,7 @@ import android.os.Handler;
 
 import com.google.gson.Gson;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class OverviewActivity extends ListActivity {
 
@@ -38,6 +40,7 @@ public class OverviewActivity extends ListActivity {
 
         //Reads all notes in DB
         MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
+        addTestData(dbHandler);
         allNotes = dbHandler.getAll();
 
         for(NoteObject no : allNotes){
@@ -125,50 +128,72 @@ public class OverviewActivity extends ListActivity {
     {
         public void handleMessage(Message msg)
         {
-            //test data :-)
-            allNotes.add(new NoteObject("TestTitle", "You can scroll the History view up and down " +
-                    "to see all of a repository's commitseven the first one ever made! " +
-                    "Select a commit from the list to reveal its individual file diffs."));
-            allNotes.add(new NoteObject("TestTitle2", "Congratulations! So far, you've learned " +
-                    "how to fork a project to your own account on GitHub, clone it in GitHub for " +
-                    "Mac so that you can make your own changes, commit and sync those changes, and " +
-                    "view the whole commit history."));
-            allNotes.add(new NoteObject("TestTitle", "You can scroll the History view up and down " +
-                    "to see all of a repository's commitseven the first one ever made! " +
-                    "Select a commit from the list to reveal its individual file diffs."));
-            allNotes.add(new NoteObject("TestTitle2", "Congratulations! So far, you've learned " +
-                    "how to fork a project to your own account on GitHub, clone it in GitHub for " +
-                    "Mac so that you can make your own changes, commit and sync those changes, and " +
-                    "view the whole commit history."));
-            allNotes.add(new NoteObject("TestTitle", "You can scroll the History view up and down " +
-                    "to see all of a repository's commitseven the first one ever made! " +
-                    "Select a commit from the list to reveal its individual file diffs."));
-            allNotes.add(new NoteObject("TestTitle2", "Congratulations! So far, you've learned " +
-                    "how to fork a project to your own account on GitHub, clone it in GitHub for " +
-                    "Mac so that you can make your own changes, commit and sync those changes, and " +
-                    "view the whole commit history."));
-            allNotes.add(new NoteObject("TestTitle", "You can scroll the History view up and down " +
-                    "to see all of a repository's commitseven the first one ever made! " +
-                    "Select a commit from the list to reveal its individual file diffs."));
-            allNotes.add(new NoteObject("TestTitle2", "Congratulations! So far, you've learned " +
-                    "how to fork a project to your own account on GitHub, clone it in GitHub for " +
-                    "Mac so that you can make your own changes, commit and sync those changes, and " +
-                    "view the whole commit history."));
-            allNotes.add(new NoteObject("TestTitle", "You can scroll the History view up and down " +
-                    "to see all of a repository's commitseven the first one ever made! " +
-                    "Select a commit from the list to reveal its individual file diffs."));
-            allNotes.add(new NoteObject("TestTitle2", "Congratulations! So far, you've learned " +
-                    "how to fork a project to your own account on GitHub, clone it in GitHub for " +
-                    "Mac so that you can make your own changes, commit and sync those changes, and " +
-                    "view the whole commit history."));
-
             //NOTE: hvis der skal hentes data fra DB/server, saa goeres det her.
             adapter = new CustomListAdapter(OverviewActivity.this, R.layout.list_notes, allNotes);
+
+//            for(int i = 0; i < allNotes.size(); i++){
+//               db.addNote(allNotes.get(i));
+//            }
+
+//            for(Iterator<NoteObject> i = allNotes.iterator(); i.hasNext();){
+//                db.addNote(i.next());
+//            }
 
             setListAdapter(adapter);
             Log.d(TAG, "HandlerSet");
         }
     };
+
+    public void addTestData(MyDBHandler dbHandler){
+        NoteObject a,b,c,d,e,f,g,h,i,j;
+
+        a = new NoteObject("TestTitle", "You can scroll the History view up and down " +
+                "to see all of a repository's commitseven the first one ever made! " +
+                "Select a commit from the list to reveal its individual file diffs.");
+        b = new NoteObject("TestTitle2", "Congratulations! So far, you've learned " +
+                "how to fork a project to your own account on GitHub, clone it in GitHub for " +
+                "Mac so that you can make your own changes, commit and sync those changes, and " +
+                "view the whole commit history.");
+        c = new NoteObject("TestTitle3", "You can scroll the History view up and down " +
+                "to see all of a repository's commitseven the first one ever made! " +
+                "Select a commit from the list to reveal its individual file diffs.");
+        d = new NoteObject("TestTitle4", "Congratulations! So far, you've learned " +
+                "how to fork a project to your own account on GitHub, clone it in GitHub for " +
+                "Mac so that you can make your own changes, commit and sync those changes, and " +
+                "view the whole commit history.");
+        e = new NoteObject("TestTitle5", "You can scroll the History view up and down " +
+                "to see all of a repository's commitseven the first one ever made! " +
+                "Select a commit from the list to reveal its individual file diffs.");
+        f = new NoteObject("TestTitle6", "Congratulations! So far, you've learned " +
+                "how to fork a project to your own account on GitHub, clone it in GitHub for " +
+                "Mac so that you can make your own changes, commit and sync those changes, and " +
+                "view the whole commit history.");
+        g = new NoteObject("TestTitle7", "You can scroll the History view up and down " +
+                "to see all of a repository's commitseven the first one ever made! " +
+                "Select a commit from the list to reveal its individual file diffs.");
+        h = new NoteObject("TestTitle8", "Congratulations! So far, you've learned " +
+                "how to fork a project to your own account on GitHub, clone it in GitHub for " +
+                "Mac so that you can make your own changes, commit and sync those changes, and " +
+                "view the whole commit history.");
+        i = new NoteObject("TestTitle9", "You can scroll the History view up and down " +
+                "to see all of a repository's commitseven the first one ever made! " +
+                "Select a commit from the list to reveal its individual file diffs.");
+        j = new NoteObject("TestTitle10", "Congratulations! So far, you've learned " +
+                "how to fork a project to your own account on GitHub, clone it in GitHub for " +
+                "Mac so that you can make your own changes, commit and sync those changes, and " +
+                "view the whole commit history.");
+
+        dbHandler.addNote(a);
+        dbHandler.addNote(b);
+        dbHandler.addNote(c);
+        dbHandler.addNote(d);
+        dbHandler.addNote(e);
+        dbHandler.addNote(f);
+        dbHandler.addNote(g);
+        dbHandler.addNote(h);
+        dbHandler.addNote(i);
+        dbHandler.addNote(j);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
