e0778d73c5f74d70bdee1e38279c39caec5d1cc7
Søren <S_englund0110@hotmail.com>
2015-05-25 19:34:10 +0200

    Possible to get location.

--- app/src/main/java/com/example/jonas/examproject/MainActivity.java
+++ app/src/main/java/com/example/jonas/examproject/MainActivity.java
@@ -2,10 +2,7 @@ package com.example.jonas.examproject;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.speech.tts.TextToSpeech;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -15,29 +12,57 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.gson.Gson;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
 
-import java.io.File;
-import java.util.Locale;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private EditText title;
     private EditText content;
     private Button submit;
+    private String cordinates;
 
     private static final String TAG = "MainActivity";
+
+    //For location
+    protected GoogleApiClient mGoogleApiClient;
+    protected Location mLastLocation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        buildGoogleApixClient();
+
         initUI();
         initButtonListener();
 
     }
+
+    private void buildGoogleApixClient() {
+        //Creates methods to be used for finding location
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+/*    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }*/
 
     public void initUI(){
         title = (EditText) findViewById(R.id.editTextTitle);
@@ -51,10 +76,9 @@ public class MainActivity extends Activity {
     }
 
     public void newNote (View view) {
-
         //Writes to DB
         MyDBHandler dbHandler = new MyDBHandler(this, null, null, 1);
-        NoteObject noteObject = new NoteObject(title.getText().toString(), content.getText().toString());
+        NoteObject noteObject = new NoteObject(title.getText().toString(), content.getText().toString() + " - Cordinates: " + cordinates );
         dbHandler.addNote(noteObject);
 
         Toast.makeText(getApplicationContext(), "Note created...",
@@ -92,5 +116,50 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    protected void onStart() {
+        super.onStart();
+        Log.d("DEBUG", "onStart()");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+    /**
+     * Runs when a GoogleApiClient object successfully connects.
+     */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // Provides a simple way of getting a device's location and is well suited for
+        // applications that do not require a fine-grained location and that do not need location
+        // updates. Gets the best and most recent location currently available, which may be null
+        // in rare cases when a location is not available.
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        Log.d("DEBUG", "data: " + LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient));
+        if (mLastLocation != null) {
+            cordinates = String.valueOf(mLastLocation.getLatitude() + " - " + String.valueOf(mLastLocation.getLongitude()));
+            Log.d("Cordinates",String.valueOf(mLastLocation.getLatitude()) + " : " + String.valueOf(mLastLocation.getLongitude()));
+        } else {
+            cordinates = "No location found";
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+        // onConnectionFailed.
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
     }
 }
