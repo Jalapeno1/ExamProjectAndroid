587e3bda1acffcf8da7cb240f36f43208a8aeb11
Jonas <jons_91@hotmail.com>
2015-05-27 10:03:35 +0200

    Notification changed
    added example of alternative notificationbuilder

--- app/src/main/java/com/example/jonas/examproject/AlarmService.java
+++ app/src/main/java/com/example/jonas/examproject/AlarmService.java
@@ -36,15 +36,48 @@ public class AlarmService extends Service {
         intent1.putExtra("ContentToEdit", content);
 
         Notification notification = new Notification(R.mipmap.ic_launcher,"Notification", System.currentTimeMillis());
+
+        //If set FLAG_ACTIVITY_CLEAR_TOP, and the activity being launched is already running in the current task,
+        // then instead of launching a new instance of that activity, all of the other
+        // activities on top of it will be closed and this Intent will be delivered to
+        // the (now on top) old activity as a new Intent.
+        //If set FLAG_ACTIVITY_SINGLE_TOP, the activity will not be launched if it is already running at the top of the history stack.
         intent1.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP| Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
+        //creates pending intent and updates current pending intent (if there is one)
         PendingIntent pendingNotificationIntent = PendingIntent.getActivity( this.getApplicationContext(),0, intent1,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //cancels notification when clicked
         notification.flags |= Notification.FLAG_AUTO_CANCEL;
+        //sets content and intent
         notification.setLatestEventInfo(this.getApplicationContext(), title, content, pendingNotificationIntent);
+        //add vibrate and sound
         notification.defaults |= Notification.DEFAULT_VIBRATE;
         notification.defaults |= Notification.DEFAULT_SOUND;
 
         mManager.notify(0, notification);
+
+
+//    private void addNotification() {
+//
+//        NotificationCompat.Builder builder =
+//                new NotificationCompat.Builder(this)
+//                        .setSmallIcon(R.mipmap.ic_launcher)
+//                        .setContentTitle("Notifications Example")
+//                        .setContentText("This is a test notification")
+//                        .setWhen();
+//
+//        Intent ni = new Intent(this, EditNoteActivity.class);
+//        ni.putExtra("Title", "sdf");
+//        ni.putExtra("Content","sd");
+//        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, ni,
+//                PendingIntent.FLAG_UPDATE_CURRENT);
+//        builder.setContentIntent(contentIntent);
+//
+//        // Add as notification
+//        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//        manager.notify(1, builder.build());
+//    }
     }
 
 
--- app/src/main/java/com/example/jonas/examproject/MainActivity.java
+++ app/src/main/java/com/example/jonas/examproject/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.jonas.examproject;
 
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
